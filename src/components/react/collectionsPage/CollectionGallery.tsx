66e8d66ed42cbd322cb5719761c7df226065d23f
mtim6921 <mtim6921@gmail.com>
2026-01-27 17:32:33 +1300

    Terms,Privacy,License Pages

--- src/components/react/collectionsPage/CollectionGallery.tsx
+++ src/components/react/collectionsPage/CollectionGallery.tsx
@@ -2,18 +2,22 @@ import { useState } from "react";
 import type { Collection } from "../UI/types";
 import { CollectionItem } from "./CollectionsPage";
 import useFetchCollection from "./useFetchCollectionImages";
+import IconButton from "../UI/IconButton";
+import Modal from "../UI/Modal";
 
 export default function CollectionGallery({collectionId}: {collectionId: string}) {
         const { category, images, loading, error } = useFetchCollection({ categoryId: collectionId, sortBy: "date" });
+        const [editMode, setEditMode] = useState(false);
     return <div>
     {loading && <div>Loading...</div>}
     {error && <div className="text-red-500">Error: {error}</div>}
     {!loading && !error && category && (
         <>
-           <h1 className="text-4xl mb-6 text-center">{category.name}</h1>  
+           <h1 className="text-4xl mb-6 text-center">{category.name}<IconButton className='ml-4' onClick={()=>setEditMode(true)} onRenderIcon={()=><span >✏️</span>}/></h1>  
       
         <div className="flex justify-start gap-10">
-           
+            <Modal isOpen={editMode} onClose={() => setEditMode(false)}>
+              </Modal>
             <AddCollectionBlock />
         </div>  
         </>
@@ -43,7 +47,7 @@ const AddCollectionBlock = () => {
 const AddNewImagesPortal = () => {
   const [tab,setTab] = useState<'upload'|'select'>('upload');
 
-  
+
   return <div>
     
   </div>
