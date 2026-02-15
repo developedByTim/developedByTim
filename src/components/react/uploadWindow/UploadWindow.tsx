32b8bd9c792610aa4ff6c1c9545e909921246d24
mtim6921 <mtim6921@gmail.com>
2026-02-15 19:20:00 +1300

    Fix btn text color

--- src/components/react/uploadWindow/UploadWindow.tsx
+++ src/components/react/uploadWindow/UploadWindow.tsx
@@ -14,7 +14,7 @@ export default function UploadWindow() {
     <>
       <button
         onClick={() => setShowModal(true)}
-  className="px-4 py-2 bg-gray-800 text-[var(--text)] rounded hover:bg-gray-900"
+  className="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-900"
       >
         Upload Image
       </button>
