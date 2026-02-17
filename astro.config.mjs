acece8e2e780d51fa7e73a3fd28bfe8b595d193b
mtim6921 <mtim6921@gmail.com>
2026-02-17 22:24:22 +1300

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
-   output: 'static', 
+  output: 'server', // enable dynamic routes
+  adapter: node(),  // Node adapter required for SSR
   integrations: [react(), tailwind()],
+  server: {
+    host: true, // required for Railway to bind to 0.0.0.0
+    port: Number(process.env.PORT) || 3000
+  }
 });
